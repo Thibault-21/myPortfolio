46c668ece623400248618c12a03879c651fb55ea
Thibault-21 <thib.dec.pro@gmail.com>
2020-03-07 15:28:31 +0100

    CV link updated

--- components/Navbar/navbar.vue
+++ components/Navbar/navbar.vue
@@ -5,7 +5,7 @@
       <nuxt-link class="routes" to="/projects">Projects</nuxt-link>
       <nuxt-link class="routes" to="/skills">Skills</nuxt-link>
       <nuxt-link class="routes" to="/about">About</nuxt-link>
-      <a href="../static/CV Thibault Decousus Vue.js.pdf" download="static/CV Thibault Decousus Vue.js.pdf" target="CV" alt="CV">CV</a>
+      <a href="../CV_Thibault_Decousus_Vuejs.pdf" download="static/CV Thibault Decousus Vue.js.pdf" target="CV" alt="CV">CV</a>
       <a href="https://github.com/Thibault-21">GitHub</a>
       <a href="https://www.linkedin.com/in/thibault-decousus/?locale=en_US">LinkedIn</a>
       <nuxt-link class="routes" to="/contact">Contact</nuxt-link>
